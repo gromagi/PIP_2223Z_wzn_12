2780795c0699256bdf7c912df37fab1843200176
gromagi <116455731+gromagi@users.noreply.github.com>
2022-11-19 19:11:10 +0100

    Update Zadanie03.java

--- src/PIP_3/Zadanie03.java
+++ src/PIP_3/Zadanie03.java
@@ -1,21 +1,22 @@
-package PIP_3;//PIP_02_Zadanie2
+package PIP_3;//PIP_02_Zadanie3
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class Zadanie02 {
+public class Zadanie03 {
     public static void main(String[] args) {
 
-        int a;
+        int x,y,z;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Wprowadz liczbe 'a' z zakresu od 1 do 6 i nacisnij ENTER:");
-        a = scan.nextInt();
-        Random rand = new Random();
-        int b = rand.nextInt (6);
-        boolean czyLiczbySaRowne;
-        czyLiczbySaRowne = a==b;
-        System.out.println("Wartosc liczby 'a' wynosi: " + a + ", wartosc liczby 'b' wynosi: " + b + "");
-        System.out.println("Czy liczby 'a' i 'b' sa rowne? --> " + czyLiczbySaRowne);
+        System.out.println("Wprowadz liczbe 'x' i nacisnij ENTER:");
+        x = scan.nextInt();
+        System.out.println("Wprowadz liczbe 'y' i nacisnij ENTER:");
+        y = scan.nextInt();
+        System.out.println("Pierwotna wartosc liczby 'x' wynosi: " + x + ", wartosc liczby 'y' wynosi: " + y);
+        z = x;
+        x = y;
+        y = z;
+        System.out.println("Ostateczna wartosc liczby 'x' wynosi: " + x + ", wartosc liczby 'y' wynosi: " + y);
+
 
 
     }
